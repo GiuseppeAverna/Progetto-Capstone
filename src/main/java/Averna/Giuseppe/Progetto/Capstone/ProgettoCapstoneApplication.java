2acd88ed30c7c4e250595e74b07470db4130437d
GiuseppeAverna <151162626+GiuseppeAverna@users.noreply.github.com>
2024-05-12 16:30:01 +0200

    collegamento backend
    collegamento backend

---
+++ src/main/java/Averna/Giuseppe/Progetto/Capstone/ProgettoCapstoneApplication.java
@@ -0,0 +1,28 @@
+package Averna.Giuseppe.Progetto.Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class ProgettoCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoCapstoneApplication.class, args);
+	}
+
+}
+//versione normale con security
+//package Averna.Giuseppe.Progetto.Capstone;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class ProgettoCapstoneApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ProgettoCapstoneApplication.class, args);
+//	}
+//
+//}
---
+++ src/test/java/Averna/Giuseppe/Progetto/Capstone/ProgettoCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package Averna.Giuseppe.Progetto.Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
